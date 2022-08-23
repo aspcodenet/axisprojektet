dc97cb299cf45eae293a5e000e7698311173bc45
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-08-23 15:23:53 +0200

    aaa

--- main.go
+++ main.go
@@ -12,7 +12,48 @@ func printMenu() {
 	fmt.Println("5. Avsluta")
 }
 
+/*
+[]string{"",""} funkar för mig. Kanske nytt i go?
+*/
+
 func main() {
+
+	arr1 := [5]int{} //not initialized
+
+	arr2 := [5]int{1, 2}          //partially initialized
+	arr3 := [5]int{1, 2, 3, 4, 5} //fully initialized
+
+	fmt.Println(arr1)
+	arr1[1] = 12334
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+
+	var cars = []string{"Volvo", "BMW", "Ford", "Mazda", "Renault"}
+	fmt.Println(cars[0])
+	fmt.Println(cars[1])
+
+	cars[3] = "Rolls Royce"
+
+	for index := 0; index < len(cars); index++ {
+		x := cars[index]
+		fmt.Println(x)
+	}
+
+	for index, carName := range cars {
+		if index == 0 {
+			fmt.Println("Detta är den första bilen")
+		}
+		fmt.Println(carName)
+	}
+
+	for _, carName := range cars {
+		fmt.Println(carName)
+	}
+
+	// var arr1 = [3]int{1,2,3}
+
+	// var spelare = [111]string{"Zlatan", "Fredrik Ljungberg"}
+
 	for {
 		var val int
 		fmt.Printf("Välj:")
