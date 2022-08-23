926683e62aa5c49271889ce012d9e186a878b6d0
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-08-23 13:38:34 +0200

    V2

--- main.go
+++ main.go
@@ -3,23 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	var val int
-	fmt.Println(`1. Skapa device
-2. Lista alla	
-3. Ändra device
-4. Sök
-5. Avsluta`)
+	for {
+		var val int
+		fmt.Println("1. Skapa device")
+		fmt.Println("2. Lista alla")
+		fmt.Println("3. Ändra device")
+		fmt.Println("4. Sök")
+		fmt.Println("5. Avsluta")
 
-	fmt.Printf("Välj:")
-	fmt.Scanln(&val) // ref
+		fmt.Printf("Välj:")
 
-	fmt.Printf("Du valde: %d", val)
+		_, e := fmt.Scanln(&val)
+		if e != nil {
+			fmt.Println("Enter a number please")
+			var discard string
+			fmt.Scanln(&discard)
+			continue
+		}
 
-	// fmt.Println("1. Skapa device")
-	// fmt.Println("2. Lista alla")
-	// fmt.Println("3. Ändra device")
-	// fmt.Println("4. Sök")
-	// fmt.Println("5. Avsluta")
+		if val < 1 || val > 5 {
+			fmt.Println("Enter a number between 1 and 5 please")
+			continue
+		}
+
+		if val == 5 {
+			break
+		}
+
+		fmt.Printf("Du valde: %d", val)
+
+	}
 
 }
 
